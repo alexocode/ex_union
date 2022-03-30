744dd7dc078c5e9d2311f11a223f326665d9a38b
Sascha Wolf <swolf.dev@gmail.com>
2022-03-30 16:02:33 +0200

    Initial Commit

---
+++ lib/ex_union.ex
@@ -0,0 +1,18 @@
+defmodule ExUnion do
+  @moduledoc """
+  Documentation for `ExUnion`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExUnion.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_union_test.exs
@@ -0,0 +1,8 @@
+defmodule ExUnionTest do
+  use ExUnit.Case
+  doctest ExUnion
+
+  test "greets the world" do
+    assert ExUnion.hello() == :world
+  end
+end
