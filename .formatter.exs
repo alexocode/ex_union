c57487be208279022e7fa23ed2091ee5dc634a79
Sascha Wolf <swolf.dev@gmail.com>
2022-03-30 19:11:52 +0200

    .formatter: Allow `defunion` to be invoked without parens

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,10 @@
 # Used by "mix format"
+locals_without_parens = [
+  defunion: 1
+]
+
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  export: [locals_without_parens: locals_without_parens],
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: locals_without_parens
 ]
